e435275576bdb52224781e937ba13d72f6fb70d7
Midory Vigueras <maestra.mevv@gmail.com>
2022-03-20 00:49:53 -0600

    Init code

---
+++ src/main/java/com/proyectotuvch/hogar/HogarExperto/HogarExpertoApplication.java
@@ -0,0 +1,13 @@
+package com.proyectotuvch.hogar.HogarExperto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogarExpertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogarExpertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectotuvch/hogar/HogarExperto/HogarExpertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectotuvch.hogar.HogarExperto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogarExpertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
